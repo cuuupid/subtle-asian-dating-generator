a83fae66176da3dcf3534963b3220ee8ade25f6a
Priansh Shah <shah@priansh.com>
2018-12-04 23:31:10 -0500

    introduce server/website

---
+++ index.py
@@ -0,0 +1,22 @@
+from sanic import Sanic
+from sanic_cors import CORS, cross_origin
+from sanic.response import text as stext
+import json
+import markovify
+import re
+
+app = Sanic(__name__)
+CORS(app)
+app.static('/', './static')
+
+posts = json.load(open('posts.json'))
+text = '\n'.join(posts)
+sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!|\n)\s', text)
+sentences = [sentence.strip() for sentence in sentences]
+mmodel = markovify.Text('\n'.join(sentences))
+
+@app.route("/generate", methods=['GET', 'OPTIONS'])
+def generate(q):
+    return stext(mmodel.make_sentence())
+
+app.run(port=6969)
